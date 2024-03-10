b594f302e1332bcf665f000388c26b64832ec0cf
Vinícius Ayres <vini.na.ayres@gmail.com>
2024-03-10 19:29:48 -0300

    Atualizar o TP01Ex10.java

--- src/TP01Ex10.java
+++ src/TP01Ex10.java
@@ -1,6 +1,8 @@
 /* 10. Entrar via teclado com o valor de uma temperatura em graus Celsius, calcular e
 exibir sua temperatura equivalente em Fahrenheit. */
 
+/* Dupla: Bianca Fonseca e Vinicius Ayres */
+
 import java.util.Locale;
 import java.util.Scanner;
 
