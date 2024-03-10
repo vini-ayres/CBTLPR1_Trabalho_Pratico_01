6652681f3fd5c7dd6cfb3691563861ae529d780c
Vinícius Ayres <vini.na.ayres@gmail.com>
2024-03-10 19:29:20 -0300

    Atualizar o TP01Ex09.java

--- src/TP01Ex09.java
+++ src/TP01Ex09.java
@@ -1,6 +1,8 @@
 /* 9. Calcular e exibir a tensão de um determinado circuito eletrônico a partir dos
 valores da resistência e corrente elétrica que serão digitados. Utilize a lei de Ohm. */
 
+/* Dupla: Bianca Fonseca e Vinicius Ayres */
+
 import java.util.Locale;
 import java.util.Scanner;
 
